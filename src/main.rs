7442bbd2ecd80025640c95e8df77d4c60b9750d5
HalloIhr0 <124879136+HalloIhr0@users.noreply.github.com>
2023-08-02 23:00:33 +0200

    Added functions to read from KeyValues

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,21 @@ use screwdriver::keyvalue::KeyValues;
 fn main() {
     let args = &env::args().collect::<Vec<String>>();
     let kv = KeyValues::parse(Path::new(&args[1])).unwrap();
-    // println!("{:#?}", kv);
+    test_get(&kv).unwrap();
     kv.write(Path::new(&args[2])).unwrap();
+}
+
+fn test_get(kv: &KeyValues) -> Option<()> {
+    println!(
+        "{}",
+        kv.get("versioninfo")?
+            .get("mapversion")?
+            .get_value()?
+    );
+    println!(
+        "{:#?}",
+        kv.get("world")?
+            .get_all("solid")
+    );
+    Some(())
 }
